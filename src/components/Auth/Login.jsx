27e7e4431b2c12bcab6de607feb0ccd49e06a635
aymanebs <aymane.bisdaoune@gmail.com>
2024-10-07 17:29:04 +0100

    AAF-4 login form and validation with react form hook done

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,54 @@
+import { useForm } from "react-hook-form";
+import AuthLayout from "../Ui/AuthLayout";
+import Button from "../Ui/Button";
+import errorIcon from "../../assets/imgs/icon-error.svg";
+import { postRequest } from "../../services/requestsService";
+
+export default function Login(){
+
+    const {register,handleSubmit,formState:{errors}} = useForm();
+
+    const Url = "login";
+    
+    const onFormSubmit = async (data) => {
+        try{
+            await postRequest(Url,data);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+    const onErrors = errors => console.error(errors);
+
+
+    return(
+        <AuthLayout
+        
+        Form={
+            <form id="form" onSubmit={handleSubmit(onFormSubmit,onErrors)}>
+
+
+            <div>
+                <input type="text" name="email" {...register("email",{required:"Email is required",pattern: { value: /\S+@\S+\.\S+/,message: "Entered value does not match email format",}})} className={`email ${errors?.email && "invalid"}`} placeholder="Email Address" id="email"  ></input>
+                {errors?.email &&
+                    <span><img src={errorIcon} alt="error" className="error-icon"/></span>
+                }
+                <span className="error">{errors?.email && errors.email.message}</span>
+            </div>
+
+            <div>
+                <input type="password" name="password" {...register("password",{required:"Password is required",type:"password"})} className={`password ${errors?.password && "invalid"}`}  placeholder="Password" id="password" ></input>
+                {errors?.password &&
+                    <span><img src={errorIcon} alt="error" className="error-icon"/></span>
+                }
+                <span className="error">{errors?.password && errors.password.message}</span>     
+             </div>
+
+            <Button/>
+
+            </form>
+        }
+        
+        />
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Register from './components/Auth/Register.jsx'
+import Login from './components/Auth/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Register />
+    {/* <Register /> */}
+    <Login />
   </StrictMode>,
 )
