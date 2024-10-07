037f35c9df43ef9d989608586078aba6ef10ade4
aymanebs <aymane.bisdaoune@gmail.com>
2024-10-07 15:55:38 +0100

    fixing merge bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Register from './components/Auth/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Register />
   </StrictMode>,
 )
